2dd2a370d9c39c2bfb0f1b712b34531e83315ed2
naveen <naveen.venugopala@gmail.com>
2023-04-02 16:40:49 +0530

    loop print stmt added

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,9 @@
 public class HelloWorld{
 
     public static void main(String[] args){
-        System.out.println("this is from git main");
-         System.out.println("made some changes ...");
+       
+       for(int i=0; i < 10; i++){
+        System.out.println("Hwllo "+ i);
+       }
     }
 }
